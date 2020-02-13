5679b64f6f5a84b7a6d40af041c051a87396bbd3
Lukas_Skywalker <lukas.diener@hotmail.com>
2020-02-13 09:11:19 +0100

    Add yarn

--- install.sh
+++ install.sh
@@ -13,6 +13,11 @@ ssh-keygen -b 2048 -t rsa -q -N "" -f ~/.ssh/id_rsa
 # NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 
+# Yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
+
 # VS Code
 sudo snap install code --classic
 
